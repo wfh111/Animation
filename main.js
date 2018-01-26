7357d206f99f046d94072b05b1f6632ba9739b92
wfh111 <wfh661@gmail.com>
2018-01-25 20:11:32 -0800

    Animation of one spritesheet

--- main.js
+++ main.js
@@ -1,35 +1,49 @@
 var AM = new AssetManager();
 
-function Animation(spriteSheet, frameWidth, frameHeight, sheetWidth, frameDuration, frames, loop, scale) {
+function Animation(spriteSheet, startX, startY, frameWidth, frameHeight, frameDuration, frames, loop, reverse) {
     this.spriteSheet = spriteSheet;
+    this.startX = startX;
+    this.startY = startY;
     this.frameWidth = frameWidth;
     this.frameDuration = frameDuration;
     this.frameHeight = frameHeight;
-    this.sheetWidth = sheetWidth;
     this.frames = frames;
     this.totalTime = frameDuration * frames;
     this.elapsedTime = 0;
     this.loop = loop;
-    this.scale = scale;
+    this.reverse = reverse;
 }
 
-Animation.prototype.drawFrame = function (tick, ctx, x, y) {
+Animation.prototype.drawFrame = function (tick, ctx, x, y, scaleBy) {
+    var scaleBy = scaleBy || 1;
     this.elapsedTime += tick;
-    if (this.isDone()) {
-        if (this.loop) this.elapsedTime = 0;
+    if (this.loop) {
+        if (this.isDone()) {
+            this.elapsedTime = 0;
+        }
+    } else if (this.isDone()) {
+        return;
     }
-    var frame = this.currentFrame();
-    var xindex = 0;
-    var yindex = 0;
-    xindex = frame % this.sheetWidth;
-    yindex = Math.floor(frame / this.sheetWidth);
+    var index = this.reverse ? this.frames - this.currentFrame() - 1 : this.currentFrame();
+    var vindex = 0;
+    if ((index + 1) * this.frameWidth + this.startX > this.spriteSheet.width) {
+        index -= Math.floor((this.spriteSheet.width - this.startX) / this.frameWidth);
+        vindex++;
+    }
+    while ((index + 1) * this.frameWidth > this.spriteSheet.width) {
+        index -= Math.floor(this.spriteSheet.width / this.frameWidth);
+        vindex++;
+    }
 
+    var locX = x;
+    var locY = y;
+    var offset = vindex === 0 ? this.startX : 0;
     ctx.drawImage(this.spriteSheet,
-                 xindex * this.frameWidth, yindex * this.frameHeight,  // source from sheet
-                 this.frameWidth, this.frameHeight,
-                 x, y,
-                 this.frameWidth * this.scale,
-                 this.frameHeight * this.scale);
+                  index * this.frameWidth + offset, vindex * this.frameHeight + this.startY,  // source from sheet
+                  this.frameWidth, this.frameHeight,
+                  locX, locY,
+                  this.frameWidth * scaleBy,
+                  this.frameHeight * scaleBy);
 }
 
 Animation.prototype.currentFrame = function () {
@@ -59,7 +73,7 @@ Background.prototype.update = function () {
 
 // inheritance 
 function Pumpkin_Death(game, spritesheet) {
-    this.animation = new Animation(spritesheet, 158, 200, 1586, .15, 10, true, .8);
+    this.animation = new Animation(spritesheet, 0, 0, 409, 325, .15, 10, true);
     this.speed = 0;
     this.ctx = game.ctx;
     Entity.call(this, game, 0, 0);
@@ -75,14 +89,15 @@ Pumpkin_Death.prototype.update = function () {
 }
 
 Pumpkin_Death.prototype.draw = function () {
-    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 0.4);
     Entity.prototype.draw.call(this);
 }
+
 function Pumpkin_Run(game, spritesheet) {
-    this.animation = new Animation(spritesheet, 157, 700, 1586, .1, 10, true, .5);
-    this.speed = 200;
+    this.animation = new Animation(spritesheet, 0, 700, 240, 300,  .1, 8, true);
+    this.speed = 230;
     this.ctx = game.ctx;
-    Entity.call(this, game, 0, 100);
+    Entity.call(this, game, 0, 150);
 }
 
 Pumpkin_Run.prototype = new Entity();
@@ -95,13 +110,68 @@ Pumpkin_Run.prototype.update = function () {
 }
 
 Pumpkin_Run.prototype.draw = function () {
-    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y);
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 0.4);
+    Entity.prototype.draw.call(this);
+}
+
+function Pumpkin_Jump(game, spritesheet) {
+    this.animation = new Animation(spritesheet, 0, 330, 243, 320,  .2, 10, true);
+    this.speed = 150;
+    this.ctx = game.ctx;
+    this.ground = 525;
+    Entity.call(this, game, 0, 450);
+}
+
+Pumpkin_Jump.prototype = new Entity();
+Pumpkin_Jump.prototype.constructor = Pumpkin_Jump;
+
+Pumpkin_Jump.prototype.update = function () {
+    this.x += this.game.clockTick * this.speed;
+    if (this.x > 630) this.x = -230;
+    if (this.animation.isDone()) {
+        this.animation.elapsedTime = 0;
+    }
+    var jumpDistance = this.animation.elapsedTime / this.animation.totalTime;
+    var totalHeight = 200;
+
+    if (jumpDistance > 0.5)
+        jumpDistance = 1 - jumpDistance;
+
+    //var height = jumpDistance * 2 * totalHeight;
+    var height = totalHeight*(-4 * (jumpDistance * jumpDistance - jumpDistance));
+    this.y = this.ground - height;
+    Entity.prototype.update.call(this);
+}
+
+Pumpkin_Jump.prototype.draw = function () {
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x + 17, this.y - 34, 0.4);
+    Entity.prototype.draw.call(this);
+}
+
+function Pumpkin_Walk(game, spritesheet) {
+    this.animation = new Animation(spritesheet, 0, 1000, 240, 300,  .2, 10, true);
+    this.speed = 100;
+    this.ctx = game.ctx;
+    Entity.call(this, game, 0, 300);
+}
+
+Pumpkin_Walk.prototype = new Entity();
+Pumpkin_Walk.prototype.constructor = Pumpkin_Walk;
+
+Pumpkin_Walk.prototype.update = function () {
+    this.x += this.game.clockTick * this.speed;
+    if (this.x > 630) this.x = -230;
+    Entity.prototype.update.call(this);
+}
+
+Pumpkin_Walk.prototype.draw = function () {
+    this.animation.drawFrame(this.game.clockTick, this.ctx, this.x, this.y, 0.4);
     Entity.prototype.draw.call(this);
 }
 
 AM.queueDownload("./img/background.jpg");
-AM.queueDownload("./img/pumpkin_death.png");
-AM.queueDownload("./img/pumpkinSprite.png")
+AM.queueDownload("./img/pumpkin.png");
+
 
 AM.downloadAll(function () {
     var canvas = document.getElementById("gameWorld");
@@ -112,8 +182,10 @@ AM.downloadAll(function () {
     gameEngine.start();
 
     gameEngine.addEntity(new Background(gameEngine, AM.getAsset("./img/background.jpg")));
-    gameEngine.addEntity(new Pumpkin_Death(gameEngine, AM.getAsset("./img/pumpkin_death.png")));
-    gameEngine.addEntity(new Pumpkin_Run(gameEngine, AM.getAsset("./img/pumpkinSprite.png")));
+    gameEngine.addEntity(new Pumpkin_Death(gameEngine, AM.getAsset("./img/pumpkin.png")));
+    gameEngine.addEntity(new Pumpkin_Jump(gameEngine, AM.getAsset("./img/pumpkin.png")));
+    gameEngine.addEntity(new Pumpkin_Run(gameEngine, AM.getAsset("./img/pumpkin.png")));
+    gameEngine.addEntity(new Pumpkin_Walk(gameEngine, AM.getAsset("./img/pumpkin.png")));
 
 
     console.log("All Done!");
